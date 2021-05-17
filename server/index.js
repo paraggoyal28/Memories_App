7752a872e8390889ee9513e5a72b75d9c8de2f5c
paraggoyal28 <2015uec1209@mnit.ac.in>
2021-05-17 10:09:05 +0530

    refactor code to get variables from env file

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use('/posts', postRoutes);
 
 // mongodb
-const CONNECTION_URL = 'mongodb+srv://ParagGoyal:Parag1@goyal123@cluster0.helhe.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const CONNECTION_URL = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.helhe.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
